3e8d8d289e97a9d490603f53bedb6b186035c87f
algoritmist <91695266+algoritmist@users.noreply.github.com>
2024-09-28 12:17:21 +0300

    feat(config): add gradle dependencies & setup docker compose (#1)
    * feat: add basic project configuration
    
    * feat: add spring data redis dependency
    
    * feat(dependencies): add docer-compose and flyway
    
    * fix: change app group
    
    * feat: add redis to docker-compose
    
    * feat: add appication docker-compose
    
    * feat: add database to spring properties
    
    * fix: group name
    
    * fix: test path group name

---
+++ src/main/kotlin/org/mapsAdvisor/mapsAdvisor/MapsAdvisorApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mapsAdvisor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MapsAdvisorApplication
+
+fun main(args: Array<String>) {
+	runApplication<MapsAdvisorApplication>(*args)
+}
